fe8cefcbba41a5de9e1b41c7b1c881cc57fbf2dc
“HibaDado” <hibadadou1@gmail.com>
2025-12-01 12:15:53 +0200

    fix: update Toaster component styles for improved visual consistency

--- src/App.tsx
+++ src/App.tsx
@@ -32,12 +32,14 @@ function App() {
           position="top-right"
           toastOptions={{
             classNames: {
-              error: 'bg-red-600 text-white border-red-700',
-              success: 'bg-green-600 text-white border-green-700',
-              warning: 'bg-yellow-600 text-white border-yellow-700',
-              info: 'bg-blue-600 text-white border-blue-700',
+              toast: 'group toast group-[.toaster]:bg-white group-[.toaster]:text-slate-950 group-[.toaster]:border-slate-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-slate-950 dark:group-[.toaster]:text-slate-50 dark:group-[.toaster]:border-slate-800',
+              error: '!bg-red-600 !text-white !border-red-700',
+              success: '!bg-green-600 !text-white !border-green-700',
+              warning: '!bg-yellow-600 !text-white !border-yellow-700',
+              info: '!bg-blue-600 !text-white !border-blue-700',
             },
           }}
+          richColors
         />
       </Router>
     </QueryClientProvider>
